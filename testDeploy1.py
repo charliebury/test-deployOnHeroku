466f628b2a86f569a25e6a8084dee0334ca41e5b
Charlie Bury <csbury@me.com>
2018-01-24 11:07:37 +0000

    minor changes to py script to return html, current time and new get location

--- testDeploy1.py
+++ testDeploy1.py
@@ -1,14 +1,18 @@
 from flask import Flask
+from datetime import datetime
+
 app = Flask(__name__)
 
 # Test script to see if I can get Heroku working
 
 
-@app.route('/doesThisWork')
-def getRequestHello():
-    return "Successful!"
-
+@app.route('/')
+def homepage():
+    the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
+    return """
+    <h1>Successful!</h1>
+    <p>Time is currently is currently {time}.</p>
+    """.format(time=the_time)
 
 if __name__ == '__main__':
-    app.debug = True
-    app.run()
+    app.run(debug=True, use_reloader=True)
